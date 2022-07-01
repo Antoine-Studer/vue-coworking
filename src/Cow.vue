bb30ae5eb5f1a155e6a129897481c3d3dab242dd
designer <astuder@simplicite.fr>
2022-07-01 13:28:19 +0200

    premier

---
+++ src/Cow.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="vue-cow">
+    <CowHeader/>
+    <CowFooter/>
+  </div>
+</template>
+
+<script>
+
+import CowHeader from './components/CowHeader.vue'
+import CowFooter from './components/CowFooter.vue'
+export default {
+  name: 'App',
+  components: { CowHeader, CowFooter }
+}
+</script>
+
+<style>
+#vue-cow {
+  font-family: Helvetica, Arial, sans-serif;
+  text-align: center;
+}
+@media screen and (min-width: 2048px) {
+  #vue-cow {
+    width: 2048px;
+    margin: 0 auto;
+  }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,29 @@
+/*  ___ _            _ _    _ _
+ * / __(_)_ __  _ __| (_)__(_) |_ ___
+ * \__ \ | '  \| '_ \ | / _| |  _/ -_)
+ * |___/_|_|_|_| .__/_|_\__|_|\__\___|
+ *             |_|
+ * This example is using the Simplicite node.js & browser JavaScript API
+ */
+import { createApp } from 'vue';
+import Cow from './Cow.vue';
+import simplicite from 'simplicite';
+
+// Explicit URL needed for a standalone deployment, set it to undefined when deploying in Simplicité
+const cfg = { url: 'https://ressources.demo.simplicite.io', debug: false };
+const app = simplicite.session(cfg);
+
+app.info('Version: ' + simplicite.constants.MODULE_VERSION);
+app.debug(app.parameters);
+
+app.login({ username: 'designer', password: 'Bonjour2003' }).then(user => {
+    app.debug('Logged in as ' + user.login);
+
+    const vueApp = createApp(Cow);
+
+    // Make the configuration and session variable available to all components
+    vueApp.config.globalProperties.$configuration = cfg;
+    vueApp.config.globalProperties.$simplicite = app;
+
+    vueApp.mount('body');
+})
