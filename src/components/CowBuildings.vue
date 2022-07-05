8c9496551d57b26dc7d1a065df0ca2ab8b0a96ad
designer <astuder@simplicite.fr>
2022-07-05 13:44:47 +0200

    pas de ralentir

--- src/components/CowBuildings.vue
+++ src/components/CowBuildings.vue
@@ -1,8 +1,9 @@
 <template>
     <div id="main_div">
         <h1>Buildings</h1>
-        <p v-if="buildings.length==0">Chargement du catalogue des batîments</p> 
-        <ul v-else>
+        <input id="search" placeholder="Search building" v-on:keyup="searchBuilding()"/>
+        <p v-if="buildings.length==0">Loading...</p> 
+        <ul id="liste_buildings" v-else>
             <li v-for="building of buildings" v-bind:key="'building-' + building.row_id" classList="building">
             
                 <div v-bind:id="'div-building-' + building.row_id">
@@ -11,8 +12,8 @@
                     <p>Opening Time: {{building.cowBuiOpeningTime}}</p>
                     <p>Closing Time: {{building.cowBuiClosingTime}}</p>
                     <div classList="type">
-                        <p>public:</p>
-                        <p>privé:</p>
+                        <p>public: </p><p v-if="building.public  == undefined">0</p><p v-else>{{building.public.length}}</p>
+                        <p>private: </p><p v-if="building.private == undefined">0</p><p v-else>{{building.private.length}}</p>
                     </div>
 
                     <button v-on:click="focusBuilding(building.row_id)">Look</button>
@@ -32,16 +33,19 @@ export default {
         return {buildings: []};
     },
     async created() {
+        //parse buildings from simplicite
         const vm = this;
         vm.buildings = await vm.$simplicite.getBusinessObject("CowBuilding").search();
         for (var i=0; i< vm.buildings.length; i++) {
-            null;
+            vm.buildings[i].private = await vm.$simplicite.getBusinessObject("CowWorkspace").search({cowWorBuiId: this.buildings[i].row_id, cowWorStatus: "PRIVATE", cowWorAvailability: true});
+            vm.buildings[i].public = await vm.$simplicite.getBusinessObject("CowWorkspace").search({cowWorBuiId: this.buildings[i].row_id, cowWorStatus: "PUBLIC", cowWorAvailability: true});
         }
-        console.log(vm.buildings);
+        
     },
     
     methods: {
         async focusBuilding(row_id) {
+            //get the rooms of the building
             const vm = this
             let div_bui = document.getElementById("div-building-" + row_id);
             let div_workspace = Object.assign(document.createElement("div"), {classList:"div_workspace"});
@@ -54,6 +58,9 @@ export default {
             }
             div_bui.appendChild(div_workspace);
         },
+        searchBuilding() {
+            
+        }
         
     }
     
--- src/components/CowInfos.vue
+++ src/components/CowInfos.vue
@@ -24,8 +24,8 @@ export default {
             }
 
             let loginRegister = function(vm,div_profil) {
-                let login = Object.assign(document.createElement("button"), {classList:"login-button",innerText:"login"});
-                let register = Object.assign(document.createElement("button"), {classList:"login-button", innerText:"register"});
+                let login = Object.assign(document.createElement("button"), {classList:"login-button",innerText:"login", id:"button_log_in"});
+                let register = Object.assign(document.createElement("button"), {classList:"login-button", innerText:"register", id:"button_register"});
                 login.addEventListener("click",function(){
                     logIn(vm,div_profil);
                 });
@@ -36,12 +36,53 @@ export default {
                 div_profil.appendChild(register);
             }
 
-            let logIn = function() {
-                console.log('ok');
+            let verifLogIn = async function(fname,lname,password, vm) {
+                let custom = vm.$simplicite.getBusinessObject("CowCustomers");
+                let customer = await custom.search({cowCusFirstName: fname, cowCusName: lname, cowCusPassword: password});
+                if (customer.length==0) {
+                    window.alert("Identifiant ou mot de passe incorrect");
+                }
+                else {
+                    console.log("connecté");
+                    let custom_id = customer.row_id;
+                    console.log(custom_id);
+                }
+
+            }
+
+
+            let logIn = async function(vm, div_profil) {
+                let test_div_reg = document.getElementById("div_inputs");
+                if (test_div_reg!= undefined) {
+                    div_profil.removeChild(test_div_reg);
+                }
+                
+                let div_log_in = Object.assign(document.createElement("div"), {id:"div_log_in"});
+                let input_fname = Object.assign(document.createElement("input"), {classList: "infos-customer", type:"text", placeholder:"first name"});
+                let input_lname = Object.assign(document.createElement("input"), {classList: "infos-customer", type:"text", placeholder:"last name"});
+                let input_password = Object.assign(document.createElement("input"), {classList: "input-customer", type:"password", placeholder:"password"});
+                let button_log = Object.assign(document.createElement("button"), {innerText: "Log in"});
+                button_log.addEventListener("click", async function(){
+                    if(input_fname.value.length == 0 || input_lname.value.length == 0 || input_password.value.length == 0) {
+                        window.alert("Enter logs");
+                    }
+                    else {
+                        await verifLogIn(input_fname.value,input_lname.value,input_password.value, vm);
+                    }
+                    
+                });
+                div_log_in.appendChild(input_fname);
+                div_log_in.appendChild(input_lname);
+                div_log_in.appendChild(input_password);
+                div_log_in.appendChild(button_log);
+                div_profil.appendChild(div_log_in);
             }
 
             let enterInfos = function(vm, div_profil) {
-                console.log("ok");
+                let test_div_log = document.getElementById("div_log_in");
+                if (test_div_log!= undefined) {
+                    div_profil.removeChild(test_div_log);
+                }
                 let input_fname = Object.assign(document.createElement("input"), {classList: "infos-customer", type:"text", placeholder:"first name"});
                 let input_lname = Object.assign(document.createElement("input"), {classList: "infos-customer", type:"text", placeholder:"last name"});
                 let input_password = Object.assign(document.createElement("input"), {classList: "input-customer", type:"password", placeholder:"password"})
@@ -71,8 +112,8 @@ export default {
                     try {
                     console.log(fname + " " + lname);
                     var client = await vm.$simplicite.getBusinessObject('CowCustomers');
-                    console.log( await getLastRowId(vm));
-                    await client.create({"row_id": await getLastRowId(vm) + 1,"cowCusName":lname,"cowCusFirstName":fname, "cowCusMail": mail, "cowCusPhoneNumber":phone, "cowCusBirthDate": date, "cowCusPassword": password});
+                    let custom_id = await getLastRowId(vm) + 1;
+                    await client.create({"row_id":custom_id,"cowCusName":lname,"cowCusFirstName":fname, "cowCusMail": mail, "cowCusPhoneNumber":phone, "cowCusBirthDate": date, "cowCusPassword": password});
                     } catch (error) {
                         try {
                             window.alert(error.messages[0]);
@@ -86,11 +127,9 @@ export default {
 
             let getLastRowId = async function(vm) {
                 vm.customers = await vm.$simplicite.getBusinessObject("CowCustomers").search();
-                console.log(vm.customers);
                 let max = 0;
                 for (var i=0; i<vm.customers;i++) {
                     var row_id = await vm.customers[i].row_id;
-                    console.log(row_id)
                     if (row_id > max) {
                         max = row_id;
                     }
